696758ad7379309a79827e5506ff6055b5506b5d
JamesOBrien2 <144615809+JamesOBrien2@users.noreply.github.com>
2025-01-21 13:34:43 +0000

    Deploying to gh-pages from @ JamesOBrien2/JamesOBrien2.github.io@67ec2a59efd8f132a7fa2a9f6b0fffe69ec9249d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,13 +442,13 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-chalcogen-bond-based-organocatalysis",
+          title: 'Chalcogen-Bond-Based Organocatalysis',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-halogen-bond-based-organocatalysis",
+          title: 'Halogen-Bond-Based Organocatalysis',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
