07e0fa6e3123563d105ae5d6bd4516315066c792
JamesOBrien2 <144615809+JamesOBrien2@users.noreply.github.com>
2025-11-10 14:26:05 +0000

    Deploying to gh-pages from @ JamesOBrien2/JamesOBrien2.github.io@0a0eec28c06adde0b162136ab2a10a9d36b3e6ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,6 +48,17 @@ ninja.data = [{
       
         title: "A post about correcting typical Gaussian16 Errors",
       
+      description: "This is a post describing the steps required for calculating Boltzmann Populations for chemcial ensembles.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/boltzmann-distribution/";
+        
+      },
+    },{id: "post-a-post-about-correcting-typical-gaussian16-errors",
+      
+        title: "A post about correcting typical Gaussian16 Errors",
+      
       description: "This is a post describing recommended steps for correcting typical Gaussian16 Errors",
       section: "Posts",
       handler: () => {
@@ -64,28 +75,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2025/nci-plots/";
-        
-      },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
     },{id: "news-published-computational-design-of-bidentate-hypervalent-iodine-catalysts-in-halogen-bond-mediated-organocatalysis-in-chemphyschem",
