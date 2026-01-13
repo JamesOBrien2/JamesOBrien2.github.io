7b9de36373697d310245b2b9826e33b13fc75fc3
JamesOBrien2 <144615809+JamesOBrien2@users.noreply.github.com>
2026-01-13 12:47:41 +0000

    Deploying to gh-pages from @ JamesOBrien2/JamesOBrien2.github.io@03e85c807962495dfeec6de971cd775f9238f2a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,17 +55,6 @@ ninja.data = [{
           window.location.href = "/blog/2026/Moore-OFerrall-Jencks-Plot/";
         
       },
-    },{id: "post-calculating-and-visualising-moore-o-39-ferrall-jencks-plots",
-      
-        title: "Calculating and visualising Moore O&#39;Ferrall Jencks Plots",
-      
-      description: "This is a post describing the steps required for calculating and visualising 2D potential energy surfaces known as Moore O&#39;Ferrall Jencks Plots.",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2026/Moore-O'Ferrall-Jencks-Plot/";
-        
-      },
     },{id: "post-calculating-rotational-barriers-1d-dihedral-scans",
       
         title: "Calculating Rotational Barriers (1D Dihedral Scans)",
